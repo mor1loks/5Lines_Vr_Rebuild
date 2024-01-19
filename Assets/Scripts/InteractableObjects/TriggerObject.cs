3552dc9a4201d8dc96523cd8677d0f2dff6dc793
SantiagaGaming <ganychev92@gmail.com>
2024-01-19 17:56:23 +0300

    Filed places added

--- Assets/Scripts/InteractableObjects/TriggerObject.cs
+++ Assets/Scripts/InteractableObjects/TriggerObject.cs
@@ -5,12 +5,12 @@ using AosSdk.Core.Utils;
 using UnityEngine;
 [RequireComponent(typeof(Collider))]
 
-public class TriggerObject : BaseObject
+public class TriggerObject : SceneObject
 {
     [SerializeField] private SceneAosObject _enterObject;
-    [SerializeField] private SceneAosObject _exitObject;
-    private void Start()
+    protected override void Start()
     {
+        base.Start();
         IsHoverable = false;
         IsClickable = false;
     }
@@ -18,14 +18,5 @@ public class TriggerObject : BaseObject
     {
         if (_enterObject != null)
             _enterObject.InvokeOnClick();
-    }
-    private void OnTriggerExit(Collider col)
-    {
-        //API api = FindObjectOfType<API>();
-        //if (api.MenuTeleport)
-        //{
-        //    if (_exitObject != null)
-        //        _exitObject.InvokeOnClick(); было для VR необходимо переписать
-        //}
     }
 }
--- Assets/aossdk/Core/Utils/WebSocketWrapper.cs
+++ Assets/aossdk/Core/Utils/WebSocketWrapper.cs
@@ -74,6 +74,7 @@ namespace AosSdk.Core.Utils
 
         private void ClientMessageReceived(string message)
         {
+            Debug.Log("MESSAGE " + message);
             AosCommand aosCommandToQueue = null;
             try
             {
