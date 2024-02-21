5c60e55baff715fde986830a8f5e4b597ba180eb
Ilya <ganychev92@gmail.com>
2024-02-21 11:33:18 +0300

    Lighting data edited

--- Assets/Scripts/AOSObjects/LeverArm.cs
+++ Assets/Scripts/AOSObjects/LeverArm.cs
@@ -7,7 +7,7 @@ public class LeverArm : AosObjectBase
     [SerializeField] private LeverarmDSP _leverarm;
 
     [AosAction(name: "Сминить положение рукоятки 0-лево 1 -право, все остальное  - середина")]
-    public void SetRotationSide(string direction)
+    public void SetRotationSide(int direction)
     {
         _leverarm.Rotate(direction);
     }
